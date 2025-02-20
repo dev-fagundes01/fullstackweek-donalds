e48a72b22da8137cb33e71904c829fd35ae260e6
Diego Fagundes <fagundesdiego2015bolcombr@gmail.com>
2025-02-20 20:23:32 -0300

    feat: add shadcn initial setup and components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import React from 'react'
+
+export default function ProductPage() {
+  return (
+    <div>
+      <h1>Product Page</h1>
+      <Button>FSW 7.0</Button>
+      <Input />
+    </div>
+  )
+}
